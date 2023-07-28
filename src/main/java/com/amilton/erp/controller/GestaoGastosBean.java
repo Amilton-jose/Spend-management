3da490a1f1dae3fd09ac9b1e754533a7da36b7bc
Amilton <amiltonjosefn2019@gmail.com>
2023-07-27 23:22:16 -0300

    criado grafico de categorias e layout de total de gastos

--- src/main/java/com/amilton/erp/controller/GestaoGastosBean.java
+++ src/main/java/com/amilton/erp/controller/GestaoGastosBean.java
@@ -2,18 +2,20 @@ package com.amilton.erp.controller;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.primefaces.context.RequestContext;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 import com.amilton.erp.model.Categoria;
 import com.amilton.erp.model.Gasto;
@@ -47,35 +49,60 @@ public class GestaoGastosBean implements Serializable {
 	private Converter categoriaConverter;
 
 	private Gasto gasto;
-	
+
 	private Map<String, BigDecimal> totalPorCategoria;
-	
+
 	private BigDecimal totalGeral;
 
-    public BigDecimal calcularTotalDeGastos() {
-        totalGeral = listaGastos.stream()
-                                .map(Gasto::getValor)
-                                .reduce(BigDecimal.ZERO, BigDecimal::add);
-        return totalGeral;
-    }
-	
+	private BarChartModel barChartModel = new BarChartModel();
 
-    public void totalDeGastosPorCategoria() {
-        totalPorCategoria = new HashMap<>();
-        
-        for (Gasto gasto : listaGastos) {
-            String categoria = gasto.getCategoria().getNome();
-            BigDecimal valorGasto = gasto.getValor();
+	@PostConstruct
+	public void init() {
+		todosGastos();
+		montarGrafico();
+	}
 
-            totalPorCategoria.merge(categoria, valorGasto, BigDecimal::add); 
-        }
-        
-    }
-    
+	public void montarGrafico() {
+		barChartModel = new BarChartModel();
+		totalPorCategoria = new HashMap<>();
 
-    public Map<String, BigDecimal> getTotalPorCategoria() {
-        return totalPorCategoria;
-    }  
+		for (Gasto gasto : listaGastos) {
+			String categoria = gasto.getCategoria().getNome();
+			BigDecimal valorGasto = gasto.getValor();
+
+			totalPorCategoria.merge(categoria, valorGasto, BigDecimal::add);
+		}
+
+		ChartSeries serieCategoria = new ChartSeries();
+		for (String categoria : totalPorCategoria.keySet()) {
+			BigDecimal totalCategoria = totalPorCategoria.get(categoria);
+			serieCategoria.set(categoria, totalCategoria);
+		}
+
+		barChartModel.addSeries(serieCategoria);
+		barChartModel.setTitle("Gráfico de Gastos por Categoria");
+	}
+
+	public BigDecimal calcularTotalDeGastos() {
+		totalGeral = listaGastos.stream().map(Gasto::getValor).reduce(BigDecimal.ZERO, BigDecimal::add);
+		return totalGeral;
+	}
+
+	public void totalDeGastosPorCategoria() {
+		totalPorCategoria = new HashMap<>();
+
+		for (Gasto gasto : listaGastos) {
+			String categoria = gasto.getCategoria().getNome();
+			BigDecimal valorGasto = gasto.getValor();
+
+			totalPorCategoria.merge(categoria, valorGasto, BigDecimal::add);
+		}
+
+	}
+
+	public Map<String, BigDecimal> getTotalPorCategoria() {
+		return totalPorCategoria;
+	}
 
 	public void prepararNovoGasto() {
 		gasto = new Gasto();
@@ -89,14 +116,14 @@ public class GestaoGastosBean implements Serializable {
 		cadastroGastoService.excluir(gasto);
 		gasto = null;
 		atualizarRegistros();
+		montarGrafico();
 		messages.info("Gasto exluído com sucesso!");
 	}
 
 	public void salvar() {
 		cadastroGastoService.salvar(gasto);
-
 		atualizarRegistros();
-
+		montarGrafico();
 		messages.info("Gasto salvo com sucesso!");
 		RequestContext.getCurrentInstance().update(Arrays.asList("frm:gastosDataTable", "frm:messages"));
 	}
@@ -162,6 +189,12 @@ public class GestaoGastosBean implements Serializable {
 		return gasto != null && gasto.getId() != null;
 	}
 
-	
-	
+	public BarChartModel getBarChartModel() {
+		return barChartModel;
+	}
+
+	public void setBarChartModel(BarChartModel barChartModel) {
+		this.barChartModel = barChartModel;
+	}
+
 }
